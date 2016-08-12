b3f5a61388d52a1b6abeee5ccdb9be59846a93fe
Rocket <Rocket1184@outlook.com>
2016-08-12 21:52:13 +0800

    add get method for favicon

--- server.js
+++ server.js
@@ -13,19 +13,26 @@ class User {
 
 var RegApiUrlUsr = /\/api\/user\/(\w+)/;
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer((request, response) => {
     console.log(request.method + ': ' + request.url);
-    if (request.method === 'GET' && RegApiUrlUsr.test(request.url)) {
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        var ans = new User(RegApiUrlUsr.exec(request.url)[1], true, 19);
-        response.write(JSON.stringify(ans));
-        response.end();
+    if (request.method === 'GET') {
+        if (RegApiUrlUsr.test(request.url)) {
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            var ans = new User(RegApiUrlUsr.exec(request.url)[1], true, 19);
+            response.write(JSON.stringify(ans));
+            response.end();
+        } else if (request.url === '/favicon.ico') {
+            fs.createReadStream('./favicon.ico').pipe(response);
+        }
+
     } else {
         response.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream("D:\\Documents\\WorkSpace\\2016.8.1 NodejsLearning\\06 Server\\404.html").pipe(response);
+        fs.createReadStream('./404.html').pipe(response);
     }
 });
 
-server.listen(8080);
+var serverPort = 2333;
 
-console.log('Server is running at http://127.0.0.1:8080/');
+server.listen(serverPort);
+
+console.log(`[Rocka Node Server] Running at http://127.0.0.1:${serverPort}/`);
