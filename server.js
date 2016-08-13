1d3ea14af2ea7f02d7a6939d67e31a608fe1726d
Rocket <Rocket1184@outlook.com>
2016-08-13 15:02:57 +0800

    remore redundant response

--- server.js
+++ server.js
@@ -3,25 +3,10 @@
 var http = require('http');
 var fs = require('fs');
 
-class User {
-    constructor(name, isMale, age) {
-        this.name = name;
-        this.isMale = isMale;
-        this.age = age;
-    }
-}
-
-var RegApiUrlUsr = /\/api\/user\/(\w+)/;
-
 var server = http.createServer((request, response) => {
     console.log(request.method + ': ' + request.url);
     if (request.method === 'GET') {
-        if (RegApiUrlUsr.test(request.url)) {
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            var ans = new User(RegApiUrlUsr.exec(request.url)[1], true, 19);
-            response.write(JSON.stringify(ans));
-            response.end();
-        } else if (request.url === '/favicon.ico') {
+        if (request.url === '/favicon.ico') {
             fs.createReadStream('./favicon.ico').pipe(response);
         } else {
             response.writeHead(200, { 'Content-Type': 'text/html' });
